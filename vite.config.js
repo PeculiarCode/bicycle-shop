ce634c0a3916fb576f81cf2a1238d420368d8336
Peculiar <1442261480@qq.com>
2024-10-15 11:10:29 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { join } from 'path'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: './',
+  base: '/bicycle-shop/',
   resolve: {
     // eslint-disable-next-line no-undef
     alias: { '@': join(__dirname, './src/') },
